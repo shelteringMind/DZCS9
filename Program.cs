7cb9b2f9e9de0e03e3e7f44580e66744a94a4412
Anton Kruglyakov <akrugs@mail.ru>
2023-04-05 17:26:11 +0500

    Задача 66

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//*/
+﻿/*/
 // Задача 64: **Выполнить с помощью рекурсии**.Задайте значение N. Напишите программу, 
 // которая выведет все натуральные числа в промежутке от N до 1. 
 // N = 5 -> "5, 4, 3, 2, 1"
@@ -14,6 +14,28 @@ string Recurs(int stop, int num)
 }
 
 Console.WriteLine($"N = {N} -> \"{Recurs(1, N)}\"");
+//*/
+
+//*/
+// Задача 66: **Выполнить с помощью рекурсии**.Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите первое число диапазона: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите последнее число диапазона: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumNumRecurs(int start, int stop)
+{
+    if(start == stop) return start;
+    return (start += SumNumRecurs(start + 1, stop));
+}
+
+if(M > 0 && N > 0) Console.WriteLine($"M = {M}; N = {N} -> {SumNumRecurs(M, N)}");
+else Console.Write("Вводимые Вами числа должны быть натуральными! Попробуйте еще раз... ");
 //*/
 
 
